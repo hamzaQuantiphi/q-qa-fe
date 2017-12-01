2b901aa52e950787dc9f76bfb156c336e208c496
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2017-12-01 23:13:26 +0530

    x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 import {ClarityModule} from 'clarity-angular'
 import { AppComponent } from './app.component';
 import { ChatScreenComponent } from './components/chat-screen/chat-screen.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+
+import {HttpClientModule} from '@angular/common/http'
+
+import {ParticlesModule} from 'angular-particle'
 
 @NgModule({
   declarations: [
@@ -11,7 +18,12 @@ import { ChatScreenComponent } from './components/chat-screen/chat-screen.compon
   ],
   imports: [
     BrowserModule,
-    ClarityModule.forRoot()
+    ClarityModule.forRoot(),
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    ParticlesModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/chat-screen/chat-screen.component.ts
+++ src/app/components/chat-screen/chat-screen.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,Output } from '@angular/core';
 
 @Component({
-  selector: 'app-chat-screen',
+  selector: 'chat-screen',
   templateUrl: './chat-screen.component.html',
-  styleUrls: ['./chat-screen.component.css']
+  styleUrls: ['./chat-screen.component.sass']
 })
 export class ChatScreenComponent implements OnInit {
 
+  @Input() 
+  set receivedQuestion(question){
+    // if(this.receivedQuestions.length == 1) {
+    //   this.receivedQuestions[0] = question
+    // } else {
+      this.receivedQuestions.push(question)
+    // }
+    
+  }
+  
+
+  receivedQuestions: string[] = []
   constructor() { }
 
   ngOnInit() {
+    console.log(this.receivedQuestion);
   }
+
+
 
 }
